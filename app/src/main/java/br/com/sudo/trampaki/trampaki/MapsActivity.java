a2d5b3728bcd5c476ad31aac3e2e70fc8548942c
Gabriel Morais Silva <gmoraizsilva@gmail.com>
2017-06-18 16:51:35 -0300

    Implementação do mapa e seu estilo.

---
+++ app/src/main/java/br/com/sudo/trampaki/trampaki/MapsActivity.java
@@ -0,0 +1,56 @@
+package br.com.sudo.trampaki.trampaki;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.map_style));
+        LatLng myHome = new LatLng(-23.9531824,-46.3948659);
+        mMap.addMarker(new MarkerOptions().position(myHome).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker)));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myHome,15));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                //CHAMA CHAMA A API.
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
